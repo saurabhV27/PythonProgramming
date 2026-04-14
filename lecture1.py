df1805ebc5713bf7f88469431c33ce5b07072295
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-14 17:43:51 +0530

    python basics

---
+++ lecture1.py
@@ -0,0 +1,31 @@
+#1. Print Sum of two number taken as input from user
+
+# num1 = int(input("Enter a number of choice :: "))
+# num2 = int(input("Enter a number of choice :: "))
+# sum = num1+num2
+# print(sum)
+
+
+#2. Print area of a square taking side as input from user
+
+# side = int(input("Enter the side of square :: "))
+# squareArea = side**2
+# print(squareArea)
+
+#3. Get average of 2 floating number taken as input from user
+
+# first = float(input("Enter first Number :: "))
+# second = float(input("Enter second number :: "))
+
+# average = (first+second)/2
+# print(average)
+
+
+#4. Print True if a>=b else print false for two number a and b taken as user input
+
+a = int(input("Enter first number :: "))
+b = int(input("Enter second number :: "))
+
+print(a>=b)
+
+
