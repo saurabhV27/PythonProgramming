2f99276511ba9341e079077d443c38543f928dfd
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-16 23:15:21 +0530

    List and tuples

---
+++ lectures/lecture3.py
@@ -0,0 +1,41 @@
+#1. WAP to ask user to enter names of their 3 favourite movies and store them in a list
+# movies = []
+# movies.insert(0,input("Enter name of the first Movie : "))
+# movies.insert(1,input("Enter name of the second Movie : "))
+# movies.insert(3,input("Enter name of the third Movie : "))
+
+# print(movies)
+# print(type(movies))
+
+#correct way is to use the append method to add elements to the list and not insert
+
+
+#2. WAP to check if list contains a palindrome of elements. Hint use copy() method
+#[1,2,3,2,1]
+# l1 = [1,'abc','abc',1,1]
+
+
+# l1Copy = l1.copy()
+# l1Copy.reverse()
+# print(l1Copy)
+# if(l1==l1Copy):
+#     print("Palindrome")
+# else:
+#     print("not a palindrome")
+
+#3. WAP to count the number of students with grade A in the following tuple.
+
+# tup = ("C","D","A","A","B","B","A",)
+# print (tup.count("A"))
+
+#4. Store the above values in a List and sort them from A to D
+
+# list = ["C","D","A","A","B","B","A"]
+
+# list.sort()
+
+# print(list)
+
+
+
+
