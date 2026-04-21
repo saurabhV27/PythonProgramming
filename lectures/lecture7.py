8433952252b6c49f288a96e481fa9341492aaddb
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-21 18:55:21 +0530

    class and objects

--- lectures/lecture7.py
+++ lectures/lecture7.py
@@ -1,3 +1,18 @@
 #1. Create a student class and take name marks of 3 subject as argument in constructor 
 #then create a method to calculate the average.
 
+# class Student:
+#     def __init__(self,name,sub1,sub2,sub3):
+#         self.name = name
+#         self.sub1 = sub1
+#         self.sub2 = sub2
+#         self.sub3 = sub3
+    
+#     def calc_Avg(self):
+#         avg = (self.sub1+self.sub2+self.sub3)/3
+#         print(avg)
+
+# s1 = Student("Karan",93,74,63)
+# s1.calc_Avg()
+
+
