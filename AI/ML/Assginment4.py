179aa0f363ac6081b562a337061a9832088f0666
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-04-29 00:25:21 +0530

    Assignment 4

---
+++ AI/ML/Assginment4.py
@@ -0,0 +1,42 @@
+#1. Check if the string given by the user is a palindrome
+
+# userInput = input("Please enter a String :: ")
+# userInput = userInput.lower()
+
+# str1 = ""
+# for ch in userInput:
+#    # print(type(ch))
+#     str1 = ch+str1
+# if str1==userInput:
+#     print(f"{str1} is a palindrome")
+# else:
+#     print(f"{userInput} is not a palindrome")
+
+#Alternate Approach
+# userInput = input("Please enter a String :: ")
+# userInput = userInput.lower()
+# userInput = userInput[::-1]
+# print(userInput)
+
+#2. Given a list of integer compute the average of the list
+
+# list=[1,5,7]
+# sum = 0
+# avg = 0
+# for val in list:
+#     sum+=val
+# avg= sum/len(list) 
+# print(f"Average is {avg}")
+
+#3. Input 2 lists from user . Merge them and sort the list
+
+# list1=[1,4,5]
+# list2=[3,6,7,9]
+
+# result = list1+list2
+# print(result)
+
+# result.sort()
+
+# print(result)
+
