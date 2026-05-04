81c11facf46cf8fb0df77b3768cb903cc0f88f81
Saurabh repos <saurabhvalunjkar@gmail.com>
2026-05-04 17:01:03 +0530

    Assignment 5

--- AI/ML/Assignment5.py
+++ AI/ML/Assignment5.py
@@ -156,56 +156,220 @@
 #  Create subclasses circle,rectangle and triangle that 
 #  override area() method
 
-class Shape:
-    def __init__(self,cir_rad=0,rec_len=0,rec_bre=0,tri_base=0,tri_hei=0):
-        self.cir_rad=cir_rad
-        self.rec_len=rec_len
-        self.rec_bre=rec_bre
-        self.tri_base=tri_base
-        self.tri_hei=tri_hei
+# class Shape:
+#     def __init__(self,cir_rad=0,rec_len=0,rec_bre=0,tri_base=0,tri_hei=0):
+#         self.cir_rad=cir_rad
+#         self.rec_len=rec_len
+#         self.rec_bre=rec_bre
+#         self.tri_base=tri_base
+#         self.tri_hei=tri_hei
     
-    def area(self):
-        pass
+#     def area(self):
+#         pass
 
-class Circle(Shape):
-    def __init__(self,cir_rad):
-        super().__init__(cir_rad=cir_rad)
-    def area(self):
-        circle_area = 3.14*(self.cir_rad**2)
-        print(f"Area of a circle :: {circle_area}")
+# class Circle(Shape):
+#     def __init__(self,cir_rad):
+#         super().__init__(cir_rad=cir_rad)
+#     def area(self):
+#         circle_area = 3.14*(self.cir_rad**2)
+#         print(f"Area of a circle :: {circle_area}")
 
-class Rectangle(Shape):
-    def __init__(self,rec_len,rec_bre):
-        super().__init__(rec_len=rec_len,rec_bre=rec_bre)
-    def area(self):
-        rectagle_area = self.rec_len*self.rec_bre
-        print(f"Area of a Rectangle :: {rectagle_area}")
+# class Rectangle(Shape):
+#     def __init__(self,rec_len,rec_bre):
+#         super().__init__(rec_len=rec_len,rec_bre=rec_bre)
+#     def area(self):
+#         rectagle_area = self.rec_len*self.rec_bre
+#         print(f"Area of a Rectangle :: {rectagle_area}")
 
-class Triangle(Shape):
-    def __init__(self,tri_base,tri_hei):
-        super().__init__(tri_base=tri_base,tri_hei=tri_hei)
-    def area(self):
-        triagle_area = (self.tri_base*self.tri_hei)/2
-        print(f"Area of a Triangle :: {triagle_area}")
+# class Triangle(Shape):
+#     def __init__(self,tri_base,tri_hei):
+#         super().__init__(tri_base=tri_base,tri_hei=tri_hei)
+#     def area(self):
+#         triagle_area = (self.tri_base*self.tri_hei)/2
+#         print(f"Area of a Triangle :: {triagle_area}")
 
-c1=Circle(5)
-c1.area()
-r1=Rectangle(6,3)
-r1.area()
-t1=Triangle(4,8)
-t1.area()
+# c1=Circle(5)
+# c1.area()
+# r1=Rectangle(6,3)
+# r1.area()
+# t1=Triangle(4,8)
+# t1.area()
+
+# #5. Create a base class Vehicle with attributes like brand and model.
+# # Create 2 subclasses Car and Bike that add extra attributes - Seats (in car)
+# # engine-cc(in bike)
+# # Concept to use ->Inheritance
+
+# class Vehicle:
+#     def __init__(self,brand,model):
+#         self.brand = brand
+#         self.model = model
+
+# class Car(Vehicle):
+#     def __init__(self,brand,model):
+#         super().__init__(brand,model)
+#         self.seats = input("Enter the type of seat :: ")
+
+#     def new_car(self):
+#         print(f"Please put {self.seats} in my {self.brand} -{self.model} car ")
 
 
-
-
-    
-
-
-    
-
-
+# class Bike(Vehicle):
+#     def __init__(self,brand,model):
+#         super().__init__(brand,model)
+#         self.engine = int(input(" Please enter the power of the bike :: "))
         
+    
+#     def power_bike(self):
+#         print(f"The bike {self.brand}- {self.model} should have {self.engine} cc engine")
+
+# c1=Car("Toyota","ignis")
+# c1.new_car()
+
+# b1=Bike("Triumph","Street Triple 765")
+# b1.power_bike()
+
+#6.Create an abstract class Employee with an abstract method calculate_salary().
+# Create sub class Intern,FullTimeEmployee and ContractEmployee that implement 
+# the method differently
+# from abc import ABC,abstractmethod
+# class Employee(ABC):
+    
+#     @abstractmethod
+#     def calculate_salary(self):
+#         pass
+
+# class InternEmp(Employee):
+#     def __init__(self):
+#         self.salary=int(input("Enter the salary for intern :: "))
+
+#     def calculate_salary(self):
+#         print(f"The salary for an intern is {self.salary}")
+
+# class ContractEmp(Employee):
+#     def __init__(self):
+#         self.salary=int(input("Enter the salary for Contract Emp :: "))
+
+#     def calculate_salary(self):
+#         print(f"The salary for an ContractEmp is {self.salary}")
+
+# class FullTimeEmp(Employee):
+#     def __init__(self):
+#         self.salary=int(input("Enter the salary for FullTime :: "))
+
+#     def calculate_salary(self):
+#         print(f"The salary for an FullTime is {self.salary}")
+
+# i1=InternEmp()
+# i1.calculate_salary()
+
+# c1=ContractEmp()
+# c1.calculate_salary()
+
+# f1 = FullTimeEmp()
+# f1.calculate_salary()
+    
+
+# #7. Crete a class Person taht allows the constructor to work with :
+# #name
+# #name+age
+# #name+age+address
+
+# #As direct constructor overloading are not allowed but we have to 
+# # use default parameters to simulate constructor overloading
+
+# class Person:
+#     def __init__(self,name,age=0,address=None):
+#         self.name=name
+#         self.age=age
+#         self.address=address
+    
+#     def person(self):
+#         return (f"Name : {self.name} Age: {self.age} Address : {self.address} ")
+    
+# p1=Person("Sav")
+# Person1 = p1.person()
+
+# p2=Person("Sav",33)
+# Person2 = p2.person()
+
+# p3=Person("Sav",33,"India")
+# Person3 = p3.person()
+
+# print(Person1)
+# print(Person2)
+# print(Person3)
+
+#8. Create a class Player with
+# a class variable player_count
+# instance variable name and level 
+
+#track how many players were created
+
+# class Player:
+#     player_count = 0
+
+#     def __init__(self,name,level):
+#         self.name = name
+#         self.level=level
+#         Player.player_count+=1
+#         self.id = Player.player_count
+
+
+#     def create_player(self):
+#         return(f"Player {self.id} -> Name : {self.name} Level : {self.level}")
+
+
+# p1= Player("Priya",2)
+# p2= Player("Nishu",4)
+
+# Player1 = p1.create_player()
+# Player2 = p2.create_player()
+
+# print(Player1)
+# print(Player2)
+
+
+#9. Create the following classes Herbivore,Carnivore and Omnivore 
+# with some attributes and methods. Then create a class Bear that inherits
+#  from all the above classes to showcase how multiple inheritance works
+
+# class Herbivore:
+#     def __init__(self,food):
+#         self.food=food
+    
+#     def animal(self):
+#         print(f"A herbivore eats {self.food} when hungry")
+
+# class Carnivore:
+#     def __init__(self,hunt):
+#         self.hunt=hunt
+    
+#     def ani(self):
+#         print(f"A carnivore hunts {self.hunt} for food")
+
+# class Omnivore:
+#     def __init__(self,eats):
+#         self.eats=eats
+#     def anim(self):
+#         print(f"An omnivore can survive on {self.eats}")
+
+# class Bear(Herbivore,Carnivore,Omnivore):
+#     def __init__(self,type,eats,hunt,food):
+#         self.type=type
+#         Herbivore.__init__(self,food)
+#         Carnivore.__init__(self,hunt)
+#         Omnivore.__init__(self,eats)
+
+#     def bear_type(self):
+#         print(f"Bear can be {self.type}")
+
+
+# b1=Bear("Brown and polar","grass","meat","Plant and Meat")
+# b1.ani()
+# b1.animal()
+# b1.bear_type()
 
 
 
-         
+
